bc7ec31aba883209249a1eaea49df776cb649797
Denys <den.ohorodnik@gmail.com>
2018-06-21 12:27:20 +0300

    Alpha version 1.0

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
         primaryStage.setTitle("Project");
-        primaryStage.setScene(new Scene(root, 400, 275));
+        primaryStage.setScene(new Scene(root, 800, 420));
         primaryStage.show();
         //hi man
     }
