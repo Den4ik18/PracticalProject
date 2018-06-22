78a36bfb05d1ce370d2bb01f91d2381377961868
Denys <den.ohorodnik@gmail.com>
2018-06-22 13:41:04 +0300

    Works only in English

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,4 +1,90 @@
 package sample;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.stage.FileChooser;
+import java.awt.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import java.io.File;
 
 public class Controller {
+
+    @FXML
+    private HBox box;
+
+    @FXML
+    private ImageView image;
+
+
+    @FXML
+    private Button loadFile;
+
+    @FXML
+    private TextArea text;
+
+    @FXML
+    private Button saveText;
+
+
+
+    @FXML
+    void save_text(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+
+        fileChooser.setInitialDirectory(new File("./text/"));
+        //Set extension filter
+        FileChooser.ExtensionFilter extFilter =
+                new FileChooser.ExtensionFilter("TXT files (*.txt)", "*.txt");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        //Show save file dialog
+        File file = fileChooser.showSaveDialog(box.getScene().getWindow());
+
+        if(file != null){
+            SaveFile(text.getText(), file);
+        }
+    }
+    private void SaveFile(String content, File file){
+        try {
+            FileWriter fileWriter;
+
+            fileWriter = new FileWriter(file);
+            fileWriter.write(content);
+            fileWriter.close();
+        } catch (IOException ex) {
+            Logger.getLogger(Controller.class
+                    .getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    @FXML
+    void load_file(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+
+        fileChooser.setTitle("Load file");
+        fileChooser.setInitialDirectory(new File("./images/"));
+
+        File selectedFile = fileChooser.showOpenDialog(box.getScene().getWindow());
+        TextHandler test;
+
+        if(selectedFile != null){
+            text.setText("");
+            test = new TextHandler(selectedFile.getAbsoluteFile().toString());
+            text.setText(test.printText());
+            image.setImage(new Image(selectedFile.toURI().toString()));
+        }else{
+            System.out.println("Image not loaded!");
+        }
+    }
 }
+
--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
         primaryStage.setTitle("Project");
-        primaryStage.setScene(new Scene(root, 400, 275));
+        primaryStage.setScene(new Scene(root, 693, 355));
         primaryStage.show();
         //hi man
     }
