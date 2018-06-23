6e4b87ff4c6db915e297835cd2ee5d05ef68641c
VovKulaka <vv122333221vv@gmail.com>
2018-06-24 00:38:45 +0300

    add main icon

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -13,8 +14,9 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
         primaryStage.setTitle("Project");
         primaryStage.setScene(new Scene(root, 700, 355));
+        primaryStage.getIcons().add(new Image("file:icon/main.png"));
         primaryStage.show();
-        //hi man
+
     }
 
 
